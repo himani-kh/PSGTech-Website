fe97e96a8ddf5f719c9ee013cc81efc49805a497
maanasags <maanasa.gs@gmail.com>
2024-06-23 19:38:29 +0530

    file set up

---
+++ src/components/header/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavbarOne from './navbarOne'
+
+export default function MyHeader() {
+  return (
+    <div>
+      <div style={{margin: 0}}><NavbarOne/></div>
+    </div>
+  )
+}
